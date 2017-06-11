736a816e43bfd04dde53a9aeff70554ab55e6520
gurudev kumar <gurudevkumar51@hotmail.com>
2017-06-11 23:03:34 +0530

    Add project files.

---
+++ JobPortalDAL/Mapper/Mapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace JobPortalDAL.Mapper
+{
+    public interface IMapper<T> where T : new()
+    {
+        T Map(IDataReader r);
+    }
+}
