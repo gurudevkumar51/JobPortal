81eca801b9556344f789b56a1f10853d5d2cae58
Tanveer <bhatia.tanveersingh@gmail.com>
2017-06-26 10:36:20 +0530

    Changes for calling API's for all jobs.

--- JobPortal/Controllers/HomeController.cs
+++ JobPortal/Controllers/HomeController.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿using JobPortalDAL.Manager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JobPortalDAL.DataAccess;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace JobPortal.Controllers
 {
     public class HomeController : Controller
     {
+        private IJob jb;
+
+        public HomeController()
+        {
+            jb = new JobRepository();
+        }
         // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> Jobs()
+        {
+            var allJobs = await jb.GetAllJobs();
+            return View("Index");
         }
     }
 }
---
+++ JobPortalDAL/DataAccess/JobRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobPortalDAL.Manager;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using JobPortalDAL.Entity;
+using JobPortalDAL.Common;
+
+namespace JobPortalDAL.DataAccess
+{
+    public class JobRepository : IJob
+    {
+        HttpClient client;
+        string url = "http://localhost:58463/api/job/";
+        public JobRepository()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public async Task<List<Job>> GetAllJobs()
+        {
+            url += "alljobs";
+            var responseData = await GenericClass.CallApi(url, null);
+            
+            
+
+            if(responseData != string.Empty)
+            {
+                var allJobs = JsonConvert.DeserializeObject<List<Job>>(responseData);
+                return allJobs;
+            }
+            return null;
+            
+        }
+       
+    }
+}
